d04604f858af9f9ef7104309eb86cf54c8a69432
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2022-08-08 10:29:45 +0400

    refactored visual metric builder

--- src/trackers/visual/metric.rs
+++ src/trackers/visual/metric.rs
@@ -1,7 +1,11 @@
+/// Auxiliary class that helps to build a metric object
+pub mod builder;
+
 use crate::distance::{cosine, euclidean};
 use crate::track::{Feature, MetricQuery, ObservationAttributes};
 use crate::track::{MetricOutput, Observation, ObservationMetric};
 use crate::trackers::kalman_prediction::TrackAttributesKalmanPrediction;
+use crate::trackers::visual::metric::builder::VisualMetricBuilder;
 use crate::trackers::visual::observation_attributes::VisualObservationAttributes;
 use crate::trackers::visual::track_attributes::VisualAttributes;
 use crate::utils::bbox::Universal2DBox;
@@ -58,106 +62,6 @@ pub struct VisualMetric {
 impl Default for VisualMetric {
     fn default() -> Self {
         VisualMetricBuilder::default().build()
-    }
-}
-
-pub struct VisualMetricBuilder {
-    visual_kind: VisualMetricType,
-    positional_kind: PositionalMetricType,
-    visual_minimal_track_length: usize,
-    visual_minimal_area: f32,
-    visual_minimal_quality_use: f32,
-    visual_minimal_quality_collect: f32,
-    visual_max_observations: usize,
-}
-
-/// By default the metric object is constructed with: Euclidean visual metric, IoU(0.3) positional metric
-/// and minimal visual track length = 3
-///
-impl Default for VisualMetricBuilder {
-    fn default() -> Self {
-        VisualMetricBuilder {
-            visual_kind: VisualMetricType::Euclidean,
-            positional_kind: PositionalMetricType::IoU(0.3),
-            visual_minimal_track_length: 3,
-            visual_minimal_area: 0.0,
-            visual_minimal_quality_use: 0.0,
-            visual_minimal_quality_collect: 0.0,
-            visual_max_observations: 5,
-        }
-    }
-}
-
-impl VisualMetricBuilder {
-    pub fn visual_metric(mut self, metric: VisualMetricType) -> Self {
-        self.visual_kind = metric;
-        self
-    }
-
-    pub fn positional_metric(mut self, metric: PositionalMetricType) -> Self {
-        if let PositionalMetricType::IoU(t) = metric {
-            assert!(
-                t > 0.0 && t < 1.0,
-                "Threshold must lay between (0.0 and 1.0)"
-            );
-        }
-        self.positional_kind = metric;
-        self
-    }
-
-    pub fn visual_minimal_track_length(mut self, length: usize) -> Self {
-        assert!(
-            length > 0,
-            "The minimum amount of visual features collected before visual metric is applied should be greater than 0."
-        );
-        self.visual_minimal_track_length = length;
-        self
-    }
-
-    pub fn visual_minimal_area(mut self, area: f32) -> Self {
-        assert!(
-            area >= 0.0,
-            "The minimum area of bbox for visual feature distance calculated and feature collected should be greater than 0."
-        );
-        self.visual_minimal_area = area;
-        self
-    }
-
-    pub fn visual_minimal_quality_use(mut self, q: f32) -> Self {
-        assert!(
-            q >= 0.0,
-            "The minimum quality of visual feature should be greater than or equal to 0.0."
-        );
-        self.visual_minimal_quality_use = q;
-        self
-    }
-
-    pub fn visual_max_observations(mut self, n: usize) -> Self {
-        self.visual_max_observations = n;
-        self
-    }
-
-    pub fn visual_minimal_quality_collect(mut self, q: f32) -> Self {
-        assert!(
-            q >= 0.0,
-            "The minimum quality of visual feature should be greater than or equal to 0.0."
-        );
-        self.visual_minimal_quality_collect = q;
-        self
-    }
-
-    pub fn build(self) -> VisualMetric {
-        VisualMetric {
-            opts: Arc::new(VisualMetricOptions {
-                visual_kind: self.visual_kind,
-                positional_kind: self.positional_kind,
-                visual_minimal_track_length: self.visual_minimal_track_length,
-                // visual_minimal_area: self.visual_minimal_area,
-                // visual_minimal_quality_use: self.visual_minimal_quality_use,
-                // visual_minimal_quality_collect: self.visual_minimal_quality_collect,
-                visual_max_observations: self.visual_max_observations,
-            }),
-        }
     }
 }
 
@@ -340,9 +244,8 @@ mod optimize {
     use crate::examples::vec2;
     use crate::track::{Observation, ObservationMetric};
     use crate::trackers::sort::SortAttributesOptions;
-    use crate::trackers::visual::metric::{
-        PositionalMetricType, VisualMetricBuilder, VisualMetricType,
-    };
+    use crate::trackers::visual::metric::builder::VisualMetricBuilder;
+    use crate::trackers::visual::metric::{PositionalMetricType, VisualMetricType};
     use crate::trackers::visual::observation_attributes::VisualObservationAttributes;
     use crate::trackers::visual::track_attributes::VisualAttributes;
     use crate::utils::bbox::{BoundingBox, Universal2DBox};
@@ -463,9 +366,8 @@ mod metric_tests {
     use crate::store::TrackStore;
     use crate::track::ObservationMetricOk;
     use crate::trackers::sort::SortAttributesOptions;
-    use crate::trackers::visual::metric::{
-        PositionalMetricType, VisualMetric, VisualMetricBuilder, VisualMetricType,
-    };
+    use crate::trackers::visual::metric::builder::VisualMetricBuilder;
+    use crate::trackers::visual::metric::{PositionalMetricType, VisualMetric, VisualMetricType};
     use crate::trackers::visual::observation_attributes::VisualObservationAttributes;
     use crate::trackers::visual::track_attributes::VisualAttributes;
     use crate::utils::bbox::BoundingBox;
---
+++ src/trackers/visual/metric/builder.rs
@@ -0,0 +1,104 @@
+use crate::trackers::visual::metric::{
+    PositionalMetricType, VisualMetric, VisualMetricOptions, VisualMetricType,
+};
+use std::sync::Arc;
+
+pub struct VisualMetricBuilder {
+    visual_kind: VisualMetricType,
+    positional_kind: PositionalMetricType,
+    visual_minimal_track_length: usize,
+    visual_minimal_area: f32,
+    visual_minimal_quality_use: f32,
+    visual_minimal_quality_collect: f32,
+    visual_max_observations: usize,
+}
+
+/// By default the metric object is constructed with: Euclidean visual metric, IoU(0.3) positional metric
+/// and minimal visual track length = 3
+///
+impl Default for VisualMetricBuilder {
+    fn default() -> Self {
+        VisualMetricBuilder {
+            visual_kind: VisualMetricType::Euclidean,
+            positional_kind: PositionalMetricType::IoU(0.3),
+            visual_minimal_track_length: 3,
+            visual_minimal_area: 0.0,
+            visual_minimal_quality_use: 0.0,
+            visual_minimal_quality_collect: 0.0,
+            visual_max_observations: 5,
+        }
+    }
+}
+
+impl VisualMetricBuilder {
+    pub fn visual_metric(mut self, metric: VisualMetricType) -> Self {
+        self.visual_kind = metric;
+        self
+    }
+
+    pub fn positional_metric(mut self, metric: PositionalMetricType) -> Self {
+        if let PositionalMetricType::IoU(t) = metric {
+            assert!(
+                t > 0.0 && t < 1.0,
+                "Threshold must lay between (0.0 and 1.0)"
+            );
+        }
+        self.positional_kind = metric;
+        self
+    }
+
+    pub fn visual_minimal_track_length(mut self, length: usize) -> Self {
+        assert!(
+            length > 0,
+            "The minimum amount of visual features collected before visual metric is applied should be greater than 0."
+        );
+        self.visual_minimal_track_length = length;
+        self
+    }
+
+    pub fn visual_minimal_area(mut self, area: f32) -> Self {
+        assert!(
+            area >= 0.0,
+            "The minimum area of bbox for visual feature distance calculated and feature collected should be greater than 0."
+        );
+        self.visual_minimal_area = area;
+        self
+    }
+
+    pub fn visual_minimal_quality_use(mut self, q: f32) -> Self {
+        assert!(
+            q >= 0.0,
+            "The minimum quality of visual feature should be greater than or equal to 0.0."
+        );
+        self.visual_minimal_quality_use = q;
+        self
+    }
+
+    pub fn visual_max_observations(mut self, n: usize) -> Self {
+        self.visual_max_observations = n;
+        self
+    }
+
+    pub fn visual_minimal_quality_collect(mut self, q: f32) -> Self {
+        assert!(
+            q >= 0.0,
+            "The minimum quality of visual feature should be greater than or equal to 0.0."
+        );
+        self.visual_minimal_quality_collect = q;
+        self
+    }
+
+    pub fn build(self) -> VisualMetric {
+        VisualMetric {
+            opts: Arc::new(VisualMetricOptions {
+                visual_kind: self.visual_kind,
+                positional_kind: self.positional_kind,
+                visual_minimal_track_length: self.visual_minimal_track_length,
+                // visual_minimal_area: self.visual_minimal_area,
+                // visual_minimal_quality_use: self.visual_minimal_quality_use,
+                // visual_minimal_quality_collect: self.visual_minimal_quality_collect,
+                visual_max_observations: self.visual_max_observations,
+            }),
+        }
+    }
+}
