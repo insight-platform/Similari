3fcb6fc2125764e5a900039d756d2cd06286d8d4
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2022-08-08 17:55:02 +0400

    implemented visual tracker python options API

--- src/trackers/visual/metric/builder.rs
+++ src/trackers/visual/metric/builder.rs
@@ -32,6 +32,58 @@ impl Default for VisualMetricBuilder {
 }
 
 impl VisualMetricBuilder {
+    pub(crate) fn visual_metric_py(&mut self, metric: VisualMetricType) {
+        self.visual_kind = metric;
+    }
+
+    pub(crate) fn positional_metric_py(&mut self, metric: PositionalMetricType) {
+        if let PositionalMetricType::IoU(t) = metric {
+            assert!(
+                t > 0.0 && t < 1.0,
+                "Threshold must lay between (0.0 and 1.0)"
+            );
+        }
+        self.positional_kind = metric;
+    }
+
+    pub(crate) fn visual_minimal_track_length_py(&mut self, length: usize) {
+        assert!(
+            length > 0,
+            "The minimum amount of visual features collected before visual metric is applied should be greater than 0."
+        );
+        self.visual_minimal_track_length = length;
+    }
+
+    pub(crate) fn visual_minimal_area_py(&mut self, area: f32) {
+        assert!(
+            area >= 0.0,
+            "The minimum area of bbox for visual feature distance calculated and feature collected should be greater than 0."
+        );
+        self.visual_minimal_area = area;
+    }
+
+    pub(crate) fn visual_minimal_quality_use_py(&mut self, q: f32) {
+        assert!(
+            q >= 0.0,
+            "The minimum quality of visual feature should be greater than or equal to 0.0."
+        );
+        self.visual_minimal_quality_use = q;
+    }
+
+    pub(crate) fn visual_max_observations_py(&mut self, n: usize) {
+        self.visual_max_observations = n;
+    }
+
+    pub(crate) fn visual_minimal_quality_collect_py(&mut self, q: f32) {
+        assert!(
+            q >= 0.0,
+            "The minimum quality of visual feature should be greater than or equal to 0.0."
+        );
+        self.visual_minimal_quality_collect = q;
+    }
+}
+
+impl VisualMetricBuilder {
     pub fn visual_metric(mut self, metric: VisualMetricType) -> Self {
         self.visual_kind = metric;
         self
--- src/trackers/visual/simple_visual/options.rs
+++ src/trackers/visual/simple_visual/options.rs
@@ -1,6 +1,7 @@
 use crate::trackers::sort::SortAttributesOptions;
 use crate::trackers::visual::metric::builder::VisualMetricBuilder;
 use crate::trackers::visual::metric::{PositionalMetricType, VisualMetric, VisualMetricType};
+use crate::trackers::visual::visual_py::PyPositionalMetricType;
 use pyo3::prelude::*;
 use std::collections::HashMap;
 use std::sync::RwLock;
@@ -12,11 +13,68 @@ pub struct VisualSortOptions {
     metric_builder: VisualMetricBuilder,
 }
 
+#[pymethods]
 impl VisualSortOptions {
+    #[new]
     pub fn new() -> Self {
         Self::default()
     }
 
+    #[pyo3(name = "max_idle_epochs", text_signature = "($self, n)")]
+    pub fn max_idle_epochs_py(&mut self, n: i64) {
+        self.max_idle_epochs = n.try_into().expect("Parameter must be a positive number");
+    }
+
+    #[pyo3(name = "history_length", text_signature = "($self, n)")]
+    pub fn history_length_py(&mut self, n: i64) {
+        self.history_length = n.try_into().expect("Parameter must be a positive number");
+    }
+
+    #[pyo3(name = "visual_metric", text_signature = "($self, metric)")]
+    pub fn visual_metric_py(&mut self, metric: VisualMetricType) {
+        self.metric_builder.visual_metric_py(metric);
+    }
+
+    #[pyo3(name = "positional_metric", text_signature = "($self, metric)")]
+    pub fn positional_metric_py(&mut self, metric: PyPositionalMetricType) {
+        self.metric_builder.positional_metric_py(metric.0);
+    }
+
+    #[pyo3(
+        name = "visual_minimal_track_length",
+        text_signature = "($self, length)"
+    )]
+    pub fn visual_minimal_track_length_py(&mut self, length: i64) {
+        self.metric_builder.visual_minimal_track_length_py(
+            length
+                .try_into()
+                .expect("Parameter must be a positive number"),
+        );
+    }
+
+    #[pyo3(name = "visual_minimal_area", text_signature = "($self, area)")]
+    pub fn visual_minimal_area_py(&mut self, area: f32) {
+        self.metric_builder.visual_minimal_area_py(area);
+    }
+
+    #[pyo3(name = "visual_minimal_quality_use", text_signature = "($self, q)")]
+    pub fn visual_minimal_quality_use_py(&mut self, q: f32) {
+        self.metric_builder.visual_minimal_quality_use_py(q);
+    }
+
+    #[pyo3(name = "visual_max_observations", text_signature = "($self, n)")]
+    pub fn visual_max_observations_py(&mut self, n: i64) {
+        self.metric_builder
+            .visual_max_observations_py(n.try_into().expect("Parameter must be a positive number"));
+    }
+
+    #[pyo3(name = "visual_minimal_quality_collect", text_signature = "($self, q)")]
+    pub fn visual_minimal_quality_collect_py(&mut self, q: f32) {
+        self.metric_builder.visual_minimal_quality_collect_py(q);
+    }
+}
+
+impl VisualSortOptions {
     pub fn build(self) -> (SortAttributesOptions, VisualMetric) {
         (
             SortAttributesOptions::new(
