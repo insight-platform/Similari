d9600fff47353ba959311fa4a0743841f024b4cd
Denis Medyantsev <44088010+denisvmedyantsev@users.noreply.github.com>
2022-08-16 14:43:31 +0700

    Install curl
    Not all base images have curl installed.

--- docker/common/install-basic-deps.sh
+++ docker/common/install-basic-deps.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+apt-get update && apt-get install -y curl python3 build-essential python3-dev python3-pip
+
 curl -o rustup.sh --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs
 sh rustup.sh -y
 source $HOME/.cargo/env
@@ -9,6 +11,6 @@ rustup update
 rustc -V
 
 cargo install cargo-chef --locked
-apt-get update && apt-get install -y python3 build-essential python3-dev python3-pip
+
 /usr/bin/python3 -m pip install --upgrade pip
 /usr/bin/python3 -m pip install --upgrade maturin~=0.13
